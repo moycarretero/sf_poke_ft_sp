3ce2355ec1a4e53d9548b664a9f1e608f406e618
María Elisa Rojas Botello <merojasbotello@gmail.com>
2022-11-21 11:47:57 +0100

    avance

--- src/Controller/PokemonController.php
+++ src/Controller/PokemonController.php
@@ -10,39 +10,18 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class PokemonController extends AbstractController {
 
-    #[Route('/pokemon')] 
-    public function getPokemon () {
-
-        $pokemon = ['name' => 'Bulbasur', 
-                    'description' => 'Este Pokémon nace con una semilla en el lomo, que brota con el paso del tiempo.',
-                    'img' => 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png',
-                    'code' => '001'
-        ];
-
+    #[Route('/pokemon/{id}')] 
+    public function getPokemon (EntityManagerInterface $doctrine, $id) {
+        $repo = $doctrine -> getRepository(Pokemon :: class);
+        $pokemon = $repo -> find($id); 
         return $this->render('Pokemons/ShowPokemon.html.twig', ['pokemon' => $pokemon]);
     }
 
     #[Route('/pokemonList')]
-    public function getPokemonList () {
+    public function getPokemonList (EntityManagerInterface $doctrine) {
 
-        $pokemonList = [
-            ['name' => 'Bulbasur', 
-            'description' => 'Este Pokémon nace con una semilla en el lomo, que brota con el paso del tiempo.',
-            'img' => 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png',
-            'code' => '001'
-            ],
-            ['name' => 'Ivysaur', 
-            'description' => 'Este Pokémon nace con una semilla en el lomo, que brota con el paso del tiempo.',
-            'img' => 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/002.png',
-            'code' => '002'
-            ],
-            ['name' => 'Venusaur', 
-            'description' => 'Este Pokémon nace con una semilla en el lomo, que brota con el paso del tiempo.',
-            'img' => 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/003.png',
-            'code' => '003'
-            ]
-        ];
-
+        $repo = $doctrine -> getRepository(Pokemon::class);
+        $pokemonList = $repo -> findAll();
         return $this->render('Pokemons/ShowPokemonList.html.twig', ['pokemonList' => $pokemonList]);
     }
 
