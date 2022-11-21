9d8056207a6f1680c6b2b7e0991d4817e144514d
Toni3101 <antonioha3101@gmail.com>
2022-11-21 10:20:48 +0100

    function add

--- src/Controller/PokemonController.php
+++ src/Controller/PokemonController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Pokemon;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PokemonController extends AbstractController {
@@ -41,5 +44,35 @@ class PokemonController extends AbstractController {
         ];
 
         return $this->render('Pokemons/ShowPokemonList.html.twig', ['pokemonList' => $pokemonList]);
+    }
+
+    #[Route('/pokemonAdd')]
+    public function addPokemon(EntityManagerInterface $doctrine) {
+        $pokemon = new Pokemon ();
+        $pokemon -> setName('Bulbasur');
+        $pokemon -> setDescription('Este Pokémon nace con una semilla en el lomo, que brota con el paso del tiempo.');
+        $pokemon -> setImage('https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png');
+        $pokemon -> setCode('001');
+
+        $pokemon2 = new Pokemon ();
+        $pokemon2 -> setName('Ivysaur');
+        $pokemon2 -> setDescription('Este Pokémon nace con una semilla en el lomo, que brota con el paso del tiempo.');
+        $pokemon2 -> setImage('https://assets.pokemon.com/assets/cms2/img/pokedex/full/002.png');
+        $pokemon2 -> setCode('002');
+
+        $pokemon3 = new Pokemon ();
+        $pokemon3 -> setName('Venusaur');
+        $pokemon3 -> setDescription('Este Pokémon nace con una semilla en el lomo, que brota con el paso del tiempo.');
+        $pokemon3 -> setImage('https://assets.pokemon.com/assets/cms2/img/pokedex/full/003.png');
+        $pokemon3 -> setCode('003');
+        
+        $doctrine -> persist($pokemon);
+        $doctrine -> persist($pokemon2);
+        $doctrine -> persist($pokemon3);
+
+        $doctrine -> flush();
+        return new Response('pokemon insertados correctamente');
+
+
     }
 }
